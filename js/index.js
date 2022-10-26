fdad5ddbc8fc071126cc0f79dc88b09ada81c006
Matheus Sena <msena2103@gmail.com>
2022-10-25 21:47:05 -0300

    Update index.js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const apikey = '3tVjv9IjcyeCBFr2pvEiIgmj19mAAqjF';
+
+var search = document.getElementById("search-button");
+var city   = document.getElementById("search-input");
+
+search.onclick = function () {
+    var city   = city.value;
+    fetch('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=' + apikey + '&q=' + city)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+};
