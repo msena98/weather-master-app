a096dc1fd0b05603e9e5054aa52a0f246855d8d4
Matheus Sena <msena2103@gmail.com>
2022-10-27 07:47:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ search.onclick = (city, apikey) => {
     city = city.value;
     fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apikey}&q=${city}`)
         .then((response) => response.json())
+        .catch(error => {
+            console.log(error); 
+          })
         .then((data) => console.log(data));
 };
