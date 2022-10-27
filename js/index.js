f049854ea4ff5e20f2c514ab284aa7508f30b30e
Matheus Sena <msena2103@gmail.com>
2022-10-26 22:09:15 -0300

    Trying use dotenv ;)

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-import _ from 'env'
-
 var search = document.getElementById("search-button");
 var city   = document.getElementById("search-input");
 
+console.log(apikey);
+
 search.onclick = function () {
-    var city   = city.value;
+    city   = city.value;
     fetch('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=' + apikey + '&q=' + city)
         .then((response) => response.json())
         .then((data) => console.log(data));
